0f096d25d20d4fe50cadbe79322925282e836673
belovattt <belovatttt@gmail.com>
2017-06-29 14:54:13 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -2,15 +2,16 @@ package ru.job4j;
 
 /**
  *Class Calculate решение задачи части 001 урок 1.
- *@autor Tatyana Belova
+ *@author Tatyana Belova
  *@since 27.06.2017
 */
 
 public class Calculate {
 	/**
-	 *Метод для вывода в консоль строки "Hello world"
+	*Метод для вывода в консоль строки "Hello world".
+	*@param args - args
 	*/
-	public static void main (String [] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
 }
