22fb0d8a109a82a78ddd9796bb1ad1353fb4f84f
belovattt <belovatttt@gmail.com>
2017-10-21 18:10:30 +0300

    5.1.2. Создать итератор четные числа [#150]

--- chapter_004/src/main/java/ru/job4j/iterator/EvenIterator.java
+++ chapter_004/src/main/java/ru/job4j/iterator/EvenIterator.java
@@ -19,17 +19,9 @@ public class EvenIterator implements Iterator {
      * если метод hasNext() для такой позиции итератора уже выполнялся и
      * следующий элемент существует,
      * pointer содержит его номер, чтобы не ходить по массиву несколько раз
-     * (массивы бывают очень длинные)
      */
     private int pointer;
-    /**
-     * выполнялся ли метод hasNext() для текущей позиции итератора
-     */
-    private boolean wasHasNext;
-    /**
-     * был ли найден следующий элемент
-     */
-    private boolean hasNextReturnTrue;
+
 
     /**
      * Конструктор обнуляет текущую позицию итератора.
@@ -40,9 +32,6 @@ public class EvenIterator implements Iterator {
     EvenIterator(int[] numbers) {
         this.numbers = numbers;
         this.pointer = 0;
-        this.wasHasNext = false;
-        this.hasNextReturnTrue = false;
-
     }
 
     /**
@@ -52,27 +41,18 @@ public class EvenIterator implements Iterator {
      *
      * @return {@code true} if the iteration has more elements
      * Если следующий элемент найден, метод меняет значение поля pointer на номер найденного элемента
-     * значение поля wasHasNext меняется на true
-     * в поле hasNextReturnTrue записывается результат поиска
      */
     @Override
     public boolean hasNext() {
         boolean result = false;
-        //если для такой позиции итератора hasNext() уже вызывался
-        if (this.wasHasNext) {
-            result = hasNextReturnTrue;
-        } else {
-            for (int i = this.pointer; i < this.numbers.length; i++) {
-                if (this.numbers[i] % 2 == 0) {
-                    result = true;
-                    this.pointer = i;
-                    break;
-                }
+        for (int i = this.pointer; i < this.numbers.length; i++) {
+            if (this.numbers[i] % 2 == 0) {
+                result = true;
+                this.pointer = i;
+                break;
             }
-            //чтобы второй раз не ходить
-            this.wasHasNext = true;
-            this.hasNextReturnTrue = result;
         }
+
         return result;
     }
 
@@ -81,33 +61,16 @@ public class EvenIterator implements Iterator {
      *
      * @return the next element in the iteration
      * @throws NoSuchElementException if the iteration has no more elements
-     *                                Метод проверяет, выполнялся ли метод hasNext() для текущей позиции итератора
-     *                                если выполнялся, то
-     *                                если результат hasNext() был false, выбрасывает исключение,
-     *                                если true - записывает в result найденный элемент и сдвигает pointer на одну позицию
-     *                                если не выполнялся, то выполняет hasNext()
      */
     @Override
     public Integer next() throws NoSuchElementException {
         int result = 0;
-        if (this.wasHasNext) {
-            if (!this.hasNextReturnTrue) {
-                throw new NoSuchElementException("No even elements");
-            } else {
-                result = this.numbers[this.pointer];
-                this.pointer++;
-            }
+        if (this.hasNext()) {
+            result = this.numbers[this.pointer];
+            this.pointer++;
         } else {
-            if (this.hasNext()) {
-                result = this.numbers[this.pointer];
-                this.pointer++;
-            } else {
-                throw new NoSuchElementException("No even elements");
-            }
+            throw new NoSuchElementException("No even elements");
         }
-        //тут еще не искали
-        this.wasHasNext = false;
-        this.hasNextReturnTrue = false;
         return result;
     }
 }
--- chapter_004/src/test/java/ru/job4j/iterator/EvenIteratorTest.java
+++ chapter_004/src/test/java/ru/job4j/iterator/EvenIteratorTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+
 public class EvenIteratorTest {
     @Test
     /**
