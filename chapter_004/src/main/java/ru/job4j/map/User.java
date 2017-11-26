2d77a8bc293e43c3d77eaf066ac61aa9e54c54b5
belovattt <belovatttt@gmail.com>
2017-11-26 20:21:22 +0300

    1. Создать модель User [#999]

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+
 /**
  * класс реализует контейнер для хранения данных об одном пользователе.
-
  */
 public class User {
     /**
@@ -21,11 +21,12 @@ public class User {
 
     /**
      * конструктор.
+     *
      * @param name
      * @param children
      * @param birthday
      */
-    public User( String name, int children, Calendar birthday){
+    public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
