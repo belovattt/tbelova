9da43d70994941619b2dc0fbbf13cc6363f4fad5
belovattt <belovatttt@gmail.com>
2017-11-26 20:17:18 +0300

    2. Не перекрывать equals hashCode [#1005]

---
+++ chapter_004/src/main/java/ru/job4j/map/MapTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    /**
+     * тест выводит содержимое карты.
+     * если в качестве ключа использованы объекты
+     * одного класса, с одинаковыми полями,
+     * методы hashCode и equals не переопределены
+     */
+    @Test
+    public void whenNotOverrideEqualsAndHashCode() {
+        Calendar c = new GregorianCalendar(2017, 11, 21);
+        User user1 = new User("Vasya", 2, c);
+        User user2 = new User("Vasya", 2, c);
+        Map map = new HashMap<User, Object>();
+        map.put(user1, "qqq");
+        map.put(user2, "aaa");
+        System.out.println(map);
+    }
+}
---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+/**
+ * класс реализует контейнер для хранения данных об одном пользователе.
+
+ */
+public class User {
+    /**
+     * имя.
+     */
+    private String name;
+    /**
+     * дети.
+     */
+    int children;
+    /**
+     * дата рождения.
+     */
+    Calendar birthday;
+
+    /**
+     * конструктор.
+     * @param name
+     * @param children
+     * @param birthday
+     */
+    public User( String name, int children, Calendar birthday){
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
